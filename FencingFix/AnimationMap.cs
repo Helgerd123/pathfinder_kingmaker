2176142b3ed9980a26faa7889383091d73621561
ZJ <ssantiagoo123@gmail.com>
2021-03-29 10:06:53 +0300

    add animation map

---
+++ FencingFix/AnimationMap.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2021 Helgerd123
+// This code is licensed under MIT license (see LICENSE for details)
+
+using Kingmaker.View.Animation;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FencingFix
+{
+    internal class AnimationMap
+    {
+        public AnimationMap(string settingsFile)
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            using StreamReader file = File.OpenText(settingsFile);
+            using JsonReader reader = new JsonTextReader(file);
+            Map = serializer.Deserialize<Dictionary<WeaponAnimationStyle, WeaponAnimationStyle>>(reader);
+        }
+
+        public WeaponAnimationStyle this[WeaponAnimationStyle index]
+        {
+            get => Map.TryGetValue(index, out WeaponAnimationStyle result) ? result : WeaponAnimationStyle.None;
+        }
+
+        private Dictionary<WeaponAnimationStyle, WeaponAnimationStyle> Map { get; }
+    }
+}
